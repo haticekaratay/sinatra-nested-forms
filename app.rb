1b2d22abd37527cec7a8fe4ea02a6946d57c2685
Hatice <haticekaraty@gmail.com>
2021-04-06 14:28:39 -0400

    commit tree 67c96ee5d23dd2f357e322306355d81158dad1d2

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get "/" do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get "/new" do
+      erb :"pirates/new"
+    end
 
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+      ships_array_of_hashes= params[:pirate][:ships]
+      ships_array_of_hashes.each do |ship_hash|
+         Ship.new(ship_hash)
+      end
+      @ships = Ship.all
+      erb :"pirates/show"
+    end
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name , :weight, :height
+
+    @@all = []
+    def initialize(hash)
+        self.name = hash[:name]
+        self.weight = hash[:weight]
+        self.height = hash[:height]
+        @@all << self 
+    end
+    
+    def self.all
+        @@all
+    end
+
 end
